f64df405ff2cf476a952a35f5e709abdd3722d56
JPd <jpd@frimastudio.com>
2018-06-20 20:38:23 -0400

    premier

---
+++ Assets/MoveCameraScript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCameraScript : MonoBehaviour
+{
+    public ScreenRecorder myRecorder;
+
+    public float distanceMax = 2f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        StartCoroutine(Rotation());
+    }
+
+    private IEnumerator Rotation()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            Vector3 rand = (UnityEngine.Random.insideUnitSphere).normalized * distanceMax;
+            rand.y = Mathf.Abs(rand.y);
+            transform.position = rand;
+            transform.LookAt(new Vector3(0,1,0));
+            myRecorder.CaptureScreenshot();
+        }
+    }
+}
